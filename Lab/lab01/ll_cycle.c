32b5c9b8441b5111a1eddeded9ba8202b2f70bb5
AlgosEng-6w <ashen4725@gmail.com>
2025-09-05 14:47:09 +0800

    lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *slow = head;
+    node *fast = head;
+    while(fast != NULL) {
+        if (fast->next == NULL || fast->next->next == NULL) {
+            return 0;
+        }
+        fast = fast->next->next;
+        slow = slow->next;
+        if(fast == slow) {
+            return 1;
+        }
+    }
     return 0;
 }
--- Lab/lab01/segfault_ex.c
+++ Lab/lab01/segfault_ex.c
@@ -3,4 +3,5 @@ int main() {
     for (int i = 0; ; i++) {
         a[i] = i;
     }
+    return 0;
 }
